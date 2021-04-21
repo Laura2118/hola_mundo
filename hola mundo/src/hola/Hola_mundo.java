556c4bd59085c1320d3e5f6626a36fc32e239394
Laura <lauramatac21@gmail.com>
2021-04-21 10:05:19 +0200

    2 cambio

--- hola mundo/src/hola/Hola_mundo.java
+++ hola mundo/src/hola/Hola_mundo.java
@@ -3,6 +3,11 @@ package hola;
 public class Hola_mundo {
 	public static void main(String[] args) {
 		//boton derecho en java class team compartir proyecto
+		//y creas un repositorio
+		
+		System.out.println("hola mundo");
+		System.out.println("2 cambio");
+		
 	}
 
 }
